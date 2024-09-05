22e163a44c12f19360086c2b91985b49fd6ccb3e
OmAr-Kader <ramooor700@gmail.com>
2024-09-05 10:43:15 +0300

    Initial Commit

---
+++ sheet/sheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  sheetApp.swift
+//  sheet
+//
+//  Created by OmAr on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct sheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
